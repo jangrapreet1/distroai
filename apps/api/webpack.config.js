6c39962435b4d0f723f7feae4c8eadb2a2ddcb1b
jangrapreet1 <jangrapreet2003@gmail.com>
2026-03-14 06:20:26 +0000

    Fix: Externalize optional bcrypt deps (aws-sdk, nock, mock-aws-s3) from webpack bundle

---
+++ apps/api/webpack.config.js
@@ -0,0 +1,18 @@
+const { composePlugins } = require('@nestjs/cli/lib/compiler/helpers/plugins-loader');
+
+module.exports = function (options) {
+    return {
+        ...options,
+        externals: [
+            ...(options.externals || []),
+            // These are optional peer deps of @mapbox/node-pre-gyp (used by bcrypt)
+            // They don't exist in production and cause webpack build failures
+            function ({ request }, callback) {
+                if (/^(aws-sdk|mock-aws-s3|nock)$/.test(request)) {
+                    return callback(null, 'commonjs ' + request);
+                }
+                callback();
+            },
+        ],
+    };
+};
